5fdad399b14886367f32d4fd014f5a5db768f4ef
komal8008 <komalkashyap356@gmail.com>
2021-12-18 18:03:05 +0530

    arithmetic operation program

---
+++ Programs/arithmatic.js
@@ -0,0 +1,15 @@
+// First program of arithmatic operations
+let a = 30;
+let b = 12;
+console.log(`A is ${a} \nB is ${b} `);
+let c;
+c = a+b;
+console.log("Sum is =",c);
+c = a-b;
+console.log("Subtraction is =",c);
+c=a*b;
+console.log("Multiplication is =",c);
+c= a/b;
+console.log("division is =",c);
+c = a%b;
+console.log("modulus is =",c);
--- Programs/hello.js
+++ Programs/hello.js
@@ -1,2 +1,5 @@
 // To run Program write node program_name
 console.log("Hello World");
+let x = "komal";
+console.log(x);
+
